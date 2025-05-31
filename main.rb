9560db7d5f5fb4066757917d0c6d8101a952485e
maddox-blay <156577870+maddox-blay@users.noreply.github.com>
2025-05-31 05:37:17 +0000

    added save game

--- main.rb
+++ main.rb
@@ -1,18 +1,60 @@
+require 'yaml'
+
 dictionary = File.readlines("google-10000-english-no-swears.txt", chomp: true)
 pick_pool = dictionary.select { |word| word.length >= 5 && word.length <= 12  }
-pick = pick_pool.sample
-guess = []
-valid_guesses = ("a".."z").to_a
-invalid_guesses = []
-mistakes = 5
 
-pick.length.times do
-  guess.push("_")
+puts "Do you want to open save file: y/n"
+choice = gets.chomp
+if choice == "y"
+  puts "1. save 1"
+  puts "2. save 2"
+  puts "3. save 3"
+  print "1/2/3: "
+  num = gets.chomp
+
+  case num
+  when '1'
+    save = YAML.load_file("save1.yml")
+  when '2'
+    save = YAML.load_file("save2.yml")
+  when '3'
+    save = YAML.load_file("save3.yml")
+  end
+
+  guess = save["current"]
+  pick = save["word"]
+  valid_guesses = save["valid"]
+  invalid_guesses = save["invalid"]
+  mistakes = save["mistake"]
+
+elsif choice == "n"
+  puts "start"
+  pick = pick_pool.sample
+  guess = []
+  valid_guesses = ("a".."z").to_a
+  invalid_guesses = []
+  mistakes = 5
+  pick.length.times do
+    guess.push("_")
+  end
+else 
+  puts "invalid"
 end
+
 puts guess.join(" ")
 
+
 while true
-  print "Type in your guess: "
+
+  details = {
+    "word" => pick,
+    "current" => guess, 
+    "mistake" => mistakes, 
+    "valid" => valid_guesses, 
+    "invalid" => invalid_guesses
+  }
+
+   print "Type in your guess: "
   user_guess = gets.chomp
   if valid_guesses.include?(user_guess) && user_guess.length == 1
     if pick.include?(user_guess) 
@@ -43,4 +85,34 @@ while true
   end
   puts guess.join(" ")
   puts "mistakes left: #{mistakes}"
+
+  puts "save:y/n"
+  option = gets.chomp
+
+  if option == 'y'
+    puts "1. save 1"
+    puts "2. save 2"
+    puts "3. save 3"
+    print "1/2/3: "
+    num = gets.chomp
+
+    case num
+      when '1'
+        File.open("save1.yml", "w") do |file|
+          file.write(details.to_yaml)
+        end
+        puts "saved to save 1"
+      when '2'
+        File.open("save2.yml", "w") do |file|
+          file.write(details.to_yaml)
+        end
+        puts "saved to save 2"
+      when '3'
+        File.open("save2.yml", "w") do |file|
+          file.write(details.to_yaml)
+        end
+        puts "saved to save 3"
+    end
+  end
+
 end
